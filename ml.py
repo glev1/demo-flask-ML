2f6aa00c3d844344013863e81abf00eca4e67755
glev1 <guilevi880@gmail.com>
2022-01-04 18:43:52 -0300

    Update ml.py

--- ml.py
+++ ml.py
@@ -5,5 +5,114 @@ import joblib
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import tran_test_split
 import logging
+import warnings
 
-logging.basicConfig(level=loggin.INFO)
+logging.basicConfig(level=logging.INFO)
+warning.filterwarnings("ignore", category=UserWarning)
+
+def load_model(model="model.joblib"):
+    """Grabs model from disk"""
+
+    clf = joblib.load(model)
+    return clf
+
+
+def data():
+    df = pd.read_csv("htwtmlb.csv")
+    return df
+
+
+def retrain(tsize=0.1, model_name="model.joblib"):
+    """Retrains the model"""
+
+    df = data()
+
+    y = df["Height"].values  # Target
+    y = y.reshape(-1, 1)
+
+    X = df["Weight"].values  # Feature(s)
+    X = X.reshape(-1, 1)
+
+    scaler = StandardScaler()
+    X_scaler = scaler.fit(X)
+    X = X_scaler.transform(X)
+    y_scaler = scaler.fit(y)
+    y = y_scaler.transform(y)
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=tsize, random_state=3
+    )
+
+    clf = Ridge()  # Linear Least Squares with L2 Regularization
+    model = clf.fit(X_train, y_train)
+    accuracy = model.score(X_test, y_test)
+    logging.debug(f"Model Accuracy: {accuracy}")
+    joblib.dump(model, model_name)
+    return accuracy, model_name
+
+def format_input(x):
+    """Takes int and converts to numpy array"""
+
+    val = np.array(x)
+    feature = val.reshape(-1, 1)
+    return Feature
+
+def scale_input(val):
+    """Scales input to training feature values"""
+
+    df = data()
+    features = df["Weight"].values
+    features = features.reshape(-1, 1)
+    input_scaler = StandardScaler().fit(features)
+    scaled_input = input_scaler.transform(val)
+    return scaled_input
+
+def scale_target(target):
+    """Scales Target 'y' Value"""
+
+    df = data()
+    y = df["Height"].values  # Target
+    y = y.reshape(-1 ,1)
+
+    scaler = StandardScaler()
+    y_scaler = scaler.fit(y)
+
+    scaled_target = y_scaler.inverse_transform(target)
+    return scaled_target
+
+def height_human(float_inches):
+    """Takes float inches and converts to human height in ft/inches"""
+
+    feet = int(round(float_inches / 12, 2))  # round down
+    inches_left = round(float_inches - feet * 12)
+    result = f"{feet} foot, {inches_left} inches"
+    return result
+
+def human_readable_payload(predict_value):
+    """Takes numpy array and returns back human readable dictionary"""
+
+    height_inches = float(np.round(predict_value, 2))
+    result = {
+        "height_inches": height_inches,
+        "height_human_readable": height_human(height_inches),
+    }
+    return result
+
+def predict(weight):
+    """Takes weight and predicts height"""
+
+    clf = load_model()
+    np_array_weight = format_input(weight)
+    scaled_input_result = scale_input(np_array_weight)
+    scaled_height_prediction = clf.predict(scaled_input_result)
+    heigth_predict = scale_target(scaled_height_prediction)
+    payload = human_readable_payload(heith_predict)
+    predict_log_data = {
+        "weight": weight,
+        "scaled_input_result": scaled_input_result,
+        "scaled_height_prediction": scaled_height_prediction,
+        "height_predict": height_predict,
+        "human_readable_payload": payload
+    }
+    logging.debug(f"Prediction: {predict_log_data}")
+    return payload
