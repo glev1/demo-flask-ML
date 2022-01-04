06d799ad86484b3d1ee7a32af501f5f6dff7430a
glev1 <guilevi880@gmail.com>
2022-01-04 13:56:08 -0300

    Create ml.py

---
+++ ml.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import Ridge
+import joblib
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import tran_test_split
+import logging
+
+logging.basicConfig(level=loggin.INFO)
